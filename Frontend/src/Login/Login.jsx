82f11aedeb269b1f32024e9b885e097a47975bc9
mubarak7785 <mubarakjinabade07@gmail.com>
2023-01-11 23:38:58 +0530

    login page added

---
+++ Frontend/src/Login/Login.jsx
@@ -0,0 +1,61 @@
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { loginfailure, loginsuccess } from '../Redux/Login_Redux/Login_Action'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+// import { store } from '../Redux/Store'
+import "./Login.css";
+
+export const Login=()=>{
+const navigate=useNavigate();  
+const [logindata,setLoginupdata]=useState({})        
+const dispatch=useDispatch(); 
+
+const handlechange=(e)=>{
+      const {id,value}=e.target;
+      setLoginupdata({...logindata,[id]:value});
+  }
+ 
+  const senddata=(e)=>{
+    e.preventDefault();
+    dispatch(loginfailure())
+      fetch("https://drab-bass-teddy.cyclic.app/login",{
+      method:"POST",
+      body:JSON.stringify(logindata),
+      headers:{"content-type":"application/json"}
+      }).then(Response=>Response.json()).then((data)=>{dispatch(loginsuccess(data));if(data.token)navigate("/"); else{alert("enter correct details")} }).catch((e)=>{dispatch(loginfailure)})
+  }
+ 
+  return (
+    <div className="login">
+     
+      <div className="form_container1">
+       
+      <div className="img-div">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnY3YdgDUtHt_nMbslCGCPXfGzfSUWoeUEMMi85_FZ3Fo03H_M5P0Quip77kN5laR7YEs&usqp=CAU" alt="" />
+          </div>
+          <h2>Login</h2>
+        <form onSubmit={senddata}>
+          <label className="label" id="lab">Email</label>
+          <br />
+          <br />
+          <input type="text" placeholder="Email address" id="email" onChange={handlechange} />
+          <br />
+          <br />
+
+          <label id="lab">Password</label>
+          <br />
+          <br />
+          <input type="password" placeholder="Password" id="password" onChange={handlechange} />
+          <br />
+          <br />
+          <h4>Don't Have an account?<Link to="/signIn">Register Here</Link></h4>
+          <br />
+          <input id="subbtn" type="submit" />
+        </form>
+      </div>
+    </div>
+  );
+
+}
