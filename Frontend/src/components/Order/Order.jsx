ce58667e4426322a057b93af830be07aeda26ee2
mubarak7785 <mubarakjinabade07@gmail.com>
2023-01-23 16:44:41 +0530

    added order complete image to the order page and made the return to home button

---
+++ Frontend/src/components/Order/Order.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+import "./Order.css"
+export const Order=()=>{
+    return <div>
+        <div className="card09">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgob1szt-fPez0CxVVPeH54X33OT5enlO-JegKu7_p19oqbT-Hh9BuDKamkKT8pianb6Y&usqp=CAU"
+        alt=""
+      />
+
+      <p className="klll">Payment Successful!</p>
+    </div>
+
+    <Link to="/"><h2 className="ppp">Return Back To Home</h2></Link>
+    
+    </div>
+}
